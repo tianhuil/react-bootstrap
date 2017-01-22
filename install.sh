5bc13242a9b18c3eb93ea64b0a60b9fd915dace9
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2017-01-22 11:48:34 -0500

    First Install

---
+++ install.sh
@@ -0,0 +1,11 @@
+# Initialize npm
+npm init
+
+# Install production dependencies
+npm install react react-dom bootstrap react-bootstrap babel-preset-react --save
+
+# Install dev dependencies
+npm install webpack css-loader style-loader file-loader url-loader babel-core \
+  babel-loader babel-preset-es2015 --save-dev
+
+
